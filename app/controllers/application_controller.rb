5f770f0f59ebecacaf1e019889c0a38993f60689
pikachu0310 <pikachu13711@gmail.com>
2024-02-15 19:04:39 +0900

    :sparkles: ?locale=ja 等のクエリパラメータをページ遷移時にも保持するように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,21 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
 
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }.merge(super)
+  end
+
   private
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
 
