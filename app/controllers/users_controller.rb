238d916600d1d0bb2a6aa612875c5a391783ae4c
pikachu0310 <pikachu13711@gmail.com>
2024-03-15 17:36:52 +0900

    :sparkles: 常に認証を求めるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user!
   def index
     @users = User.order(:name).page(params[:page])
   end
