36522af238d50b880158c9aff1bc5655b79fd618
pikachu0310 <pikachu13711@gmail.com>
2024-03-15 17:16:12 +0900

    :sparkles: User情報を増やすmigrationファイルを追加

---
+++ db/migrate/20240315081350_add_details_to_users.rb
@@ -0,0 +1,8 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :postal_code, :string
+    add_column :users, :address, :string
+    add_column :users, :introduction, :text
+    add_column :users, :name, :string
+  end
+end
