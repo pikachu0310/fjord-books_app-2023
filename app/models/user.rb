8345a61a2a757d7cee9b9ec242b70c5f9168ef7a
pikachu0310 <pikachu13711@gmail.com>
2024-03-21 20:20:42 +0900

    :sparkles: user model に avatar を追加し、thumbにリサイズも保存

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar do |image|
+    image.variant :thumb, resize_to_limit: [256, 256]
+  end
 end
