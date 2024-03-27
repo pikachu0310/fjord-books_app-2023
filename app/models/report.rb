286bad82aa5de4539900ea309cd761656377d8a8
pikachu0310 <pikachu13711@gmail.com>
2024-03-27 18:55:52 +0900

    :sparkles: reportにuserのバリデーションを追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,15 @@
 
 class Report < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def editable_by?(target_user)
+    user && user == target_user
+  end
+
+  def deletable_by?(target_user)
+    user && user == target_user
+  end
 end
