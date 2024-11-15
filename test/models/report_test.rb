8d520ba7f5c4e1c590c61708ac662c5bae603c64
pikachu0310 <pikachu13711@gmail.com>
2024-11-15 23:04:38 +0900

    :sparkles: models のテストを実装

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable?' do
+    me = User.create!(email: 'me@example.com', name: 'me', password: 'password')
+    she = User.create!(email: 'she@example.com', name: 'she', password: 'password')
+
+    report = Report.create!(user: me, title: 'test', content: 'test')
+
+    assert report.editable?(me), 'The report should be editable by its creator'
+    assert_not report.editable?(she), 'The report should not be editable by others'
+  end
+
+  test 'created_on' do
+    user = User.create!(email: 'user@example.com', name: 'user', password: 'password')
+    report = Report.create!(user:, title: 'test', content: 'test')
+
+    assert_equal Time.zone.today, report.created_at.to_date, 'The report should be created with today\'s date'
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email returns email when name is empty' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email, 'Expected email to be returned when name is empty'
+  end
+
+  test '#name_or_email returns name when name is present' do
+    user = User.new(email: 'foo@example.com', name: 'Foo Bar')
+    assert_equal 'Foo Bar', user.name_or_email, 'Expected name to be returned when name is present'
+  end
 end
